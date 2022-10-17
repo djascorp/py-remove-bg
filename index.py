496f5e208a4ea1eb0fc1afef0a40b54bc8a1d74f
djasnive <djasniveyrajaona@gmail.com>
2022-10-17 14:23:26 +0300

    feat: add new homepage
    Add home.html
    home.html contains Tailwind cdn

--- index.py
+++ index.py
@@ -1,12 +1,11 @@
-from flask import Flask
-
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return 'Home Page Route'
+    return render_template('templates/home.html')
 
 
 @app.route('/about')
